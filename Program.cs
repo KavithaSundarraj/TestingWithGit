65d74103d6a83e28ba75ca2f7164a3a5147bfe35
Codecoala81 <62188788+Codecoala81@users.noreply.github.com>
2020-11-09 10:52:53 +0100

    Wrote a new comment

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TestingWithGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //New comment from Andreas
         }
     }
 }
